4fedf0a25d57c3ff948c5a781ea0c160409e14b4
Alexia Riccardi <alexiariccardi@gmail.com>
2024-09-15 13:10:43 -0400

    e1

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,29 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+
+const chipotle = {
+    name: "Chipotle",
+    cuisine: ["Mexican"],
+    numberOfStars: 4,
+    favorited: true,
+};
+
+// console.log(chipotle);
+
+chipotle.address = "555 hypoluxo rd";
+chipotle.zipcode = 33463;
+chipotle.acceptsReservations = false;
+chipotle.numberOfStars += 1;
+chipotle.favorited = false;
+chipotle.cuisine.push("American");
+
+// console.log(chipotle);
+
+function retrieveProperty(object, key){
+    if (key in object){
+        return object[key];
+    } else {return "The information you requested does not exist."}
+}
+
+console.log(retrieveProperty(chipotle, "state"));
